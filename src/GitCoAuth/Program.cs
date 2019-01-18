2f258a47e07f06dd5d5b1b4f53563eb06503f401
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2019-01-18 12:24:34 +0100

    Automatically attempt copy to clipboard

--- src/GitCoAuth/Program.cs
+++ src/GitCoAuth/Program.cs
@@ -13,14 +13,22 @@ namespace GitCoAuth {
 
             (var name, var email) = GetUserData(args[0]);
 
-            Console.Out.WriteLine("Co-authored-by: {0} <{1}>", name, email);
+            var output = string.Format("Co-authored-by: {0} <{1}>", name, email);
+
+            try {
+                TextCopy.Clipboard.SetText(output);
+            }
+            catch(Exception ex) {
+                Console.Error.WriteLine("Failed to copy to clipboard ({0})", ex.Message);
+            }
+            Console.Out.WriteLine(output);
 
             return 0;
         }
 
         private static void PrintHelp() {
             Console.Error.WriteLine("  gitcoauth <username>");
-            Console.Error.WriteLine("Prints out a commit message trailer that indicates that GitHub user <username> contributed to a commit.");
+            Console.Error.WriteLine("Prints out a commit message trailer that indicates that GitHub user <username> contributed to a commit (and copied it to clipboard, if possible).");
         }
 
         private static (string Name, string Email) GetUserData(string username) {
